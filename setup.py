4e6c699668df35289510de39f0c06e410327ad93
leogregianin <leogregianin@gmail.com>
2021-11-11 10:51:27 -0400

    Added setup for package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import setuptools
+
+setuptools.setup(
+    name='stock_exchange_holidays',
+    version='0.1',
+    author='Leonardo Gregianin',
+    author_email='leogregianin@gmail.com',
+    description='Worldwide Stock Exchange holidays (NYSE, CME, B3 and others)',
+    url='https://github.com/leogregianin',
+    zip_safe=False,
+    python_requires='>=3.6',
+    license='MIT',
+)
